0818466b06f5af9a58fee1b843febcc0a32bbfb7
thecodeplanner <thecodeplanner@gmail.com>
2020-12-16 20:54:14 -0500

    commit tree 0b0ac6ce713f54e5b995b4296f527053d6858654

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const superbowlWin = record => {
+  let results = record.find(object => {
+      if (object.result === "W") {
+        return object.year
+      }
+  })
+}
+
+// superbowlWin = (record) => {
+//   let result = record.find( record => record.result === "W" )
+//   return !!result ? result.year : undefined
+// }
